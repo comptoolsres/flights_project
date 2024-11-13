2fe48f86cebf7333c1a9c05429c5cbc0f5cf23cc
Matt Gitzendanne <magitz@ufl.edu>
2024-11-13 09:59:30 -0500

    1st iteration

--- flights.py
+++ flights.py
@@ -21,6 +21,14 @@ args = parser.parse_args()
 
 
 # Read the data from the CSV file
-data = pd.read_csv(args.filename)
+df = pd.read_csv(args.filename)
 
+# Using the Year and Month columns, calculate the number of flights each month
+# Hint: Use the groupby() function
+flights_per_month = df.groupby(['Year', 'Month']).size()
+print(flights_per_month)
 
+# Using the Year, Month and ArrDelay columns, calculate the number of flights delayed per month
+# Hint: Use the groupby() function  
+delayed_flights_per_month = df[df['ArrDelay'] > 0].groupby(['Year', 'Month']).size()
+print(delayed_flights_per_month)
